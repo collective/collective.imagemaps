843ef0f23ec7d78af64f35b404ae50d62ac24986
MrTango <md@derico.de>
2021-01-22 15:34:15 +0200

    Preparing release 1.0b1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.imagemaps',
-    version='1.0a1',
+    version='1.0b1',
     description="Provides image map behavior for Dexterity types.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
