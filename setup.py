eb1d2d0b04f1193b1ef03b26c01a020447f8aebe
MrTango <md@derico.de>
2022-04-14 16:53:29 +0300

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.imagemaps',
-    version='1.0',
+    version='1.1.dev0',
     description="Provides image map behavior for Dexterity types.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
